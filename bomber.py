a1c8c8e864f3e2c18627c85360a6a5ae03ecd53d
ANON00ANONYMOUS <37286453+anon00anonymous@users.noreply.github.com>
2019-10-31 17:46:51 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -231,11 +231,14 @@ country_codes = {
 
 def banner():
     cprint("""                                                  
-  BBBBB   LL        AAA    CCCCC  KK  KK LL        AAA   BBBBB   EEEEEEE LL      
-BB   B  LL       AAAAA  CC    C KK KK  LL       AAAAA  BB   B  EE      LL      
-BBBBBB  LL      AA   AA CC      KKKK   LL      AA   AA BBBBBB  EEEEE   LL      
-BB   BB LL      AAAAAAA CC    C KK KK  LL      AAAAAAA BB   BB EE      LL      
-BBBBBB  LLLLLLL AA   AA  CCCCC  KK  KK LLLLLLL AA   AA BBBBBB  EEEEEEE LLLLLLL         
+    (                         )  (                        (     
+   (   )\ )    (        (     ( /(  )\ )    (       (        )\ )  
+ ( )\ (()/(    )\       )\    )\())(()/(    )\    ( )\  (   (()/(  
+ )((_) /(_))((((_)(   (((_) |((_)\  /(_))((((_)(  )((_) )\   /(_)) 
+((_)_ (_))   )\ _ )\  )\___ |_ ((_)(_))   )\ _ )\((_)_ ((_) (_))   
+ | _ )| |    (_)_\(_)((/ __|| |/ / | |    (_)_\(_)| _ )| __|| |    
+ | _ \| |__   / _ \   | (__   ' <  | |__   / _ \  | _ \| _| | |__  
+ |___/|____| /_/ \_\   \___| _|\_\ |____| /_/ \_\ |___/|___||____|          
                                          """, 'green')
     print()
 
